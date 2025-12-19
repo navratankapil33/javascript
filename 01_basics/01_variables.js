5b3f5477677f38898440b7e530c2a04c22b07cda
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-19 19:02:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 14435
+let room = 234511;
+var accountPassword = "12345"
+
+accountCity = "Jaipur"
+let acco
+var rub
+// const rumic
+
+// accountId = 2;
+room = 2
+console.log(room)
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log(accountEmail)
+var accountEmail = "unknown@gmail.com"
+console.log(accountEmail)
+console.log(acco)
+console.log(rub)
+// console.log(rumic)
