ff08d3999d0397686337d1b171843ca29c3e579d
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-20 18:13:13 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,26 @@
 "use strict" // treat all JS code as newer version
+
+//alert(3+3) // we are using node js, not browser
+
+console.log(3+3)
+
+console.log("Founded and grounded by six")
+
+let name = "navratan"
+let age = 18
+let state
+let isLoggedIn = false
+
+// Number -> 2 to power 53
+// BigInt
+// Boolean -> true/false
+// String -> ""
+// null -> standalone value
+// undefined -> not defined
+// Symbol -> unique
+
+// object
+
+console.log(typeof isLoggedIn)
+console.log(typeof null) // Object
+console.log(typeof undefined) // undefined
