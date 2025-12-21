447b73faef4160f88a37035f74bde0c4f18dfb16
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-21 07:10:13 +0000

    conversion of number to other datatypes

---
+++ 01_basics/03_containeroperations.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valuetoNumber = Number(score)
+// console.log(typeof valuetoNumber)
+// console.log(valuetoNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined
+// null => 0
+
+let loggedIn = "hy";
+
+let booleanLoggedIn = Boolean(loggedIn);
+// console.log(typeof booleanLoggedIn)
+// console.log(booleanLoggedIn)
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber)
