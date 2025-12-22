8dd82bfe88f65f3ad848ccef151a3587e7159840
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-22 11:22:51 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log("2" > 3)
+// console.log("02" > 1)
+// console.log(23 > "3")
+
+
+// console.log(null > 0)
+// console.log(null >= 0)
+// console.log(null == 0)
+console.log(null == undefined)
+console.log(null === undefined)
