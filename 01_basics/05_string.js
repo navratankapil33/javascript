37fc2d30093d07a2973de663671ef6397cd5e18e
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-23 10:46:23 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,37 @@
+const name = "Navratan"
+const repo = 50
+
+// console.log(name + repo + " Value")
+
+// console.log(`My name is ${name} and repository count is ${repo}`) // String Interpolation
+
+const gameName = new String("yours daddy")
+// console.log(gameName)
+
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(6))
+
+// console.log(gameName.indexOf('r'))
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+// const anotherString = gameName.slice(0,3)
+// console.log(anotherString)
+// const checkString = "   navubaby    "
+// console.log(checkString)
+// console.log(checkString.trim())
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url)
+console.log(url.replace("%20","-"))
+
+const forName = "robin-malik-rutba"
+
+const newName = forName.split('-')
+
+console.log(newName)
