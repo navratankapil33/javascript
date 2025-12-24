d0da6e490576c5ab76e880323e4cd77292521b43
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-24 06:28:49 +0000

    numandmath

---
+++ 01_basics/06_numberandmaths.js
@@ -0,0 +1,33 @@
+const score = 400
+
+// console.log(score)
+
+const balance = new Number(100)
+
+// console.log(balance)
+
+// console.log(balance.toFixed(3))
+
+// console.log(balance.toString().length)
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-In'))
+
+const maxValue = Number.MAX_VALUE
+
+// console.log(maxValue)
+
+const minValue = Number.MIN_VALUE
+
+// console.log(minValue)
+
+// +++++++++++++++++ Maths ++++++++++++++++++++++++
+
+console.log(Math)
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.3))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
