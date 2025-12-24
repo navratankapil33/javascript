f5974b70d716b1e8403976073c3cf2fadf11186f
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-24 12:33:18 +0000

    dateandtime

---
+++ 01_basics/07_dateandtime.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+
+
+console.log(typeof myDate)
+
+let myCreatedDate = new Date(2025,11,24)
+
+console.log(myCreatedDate)
+console.log(myCreatedDate.getTime())
+
+let timeStamp = Date.now()
+console.log(timeStamp)
