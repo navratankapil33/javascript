881fd7b49f56aea486fab9575ea5a2ef2e0c993c
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-26 11:19:26 +0000

    object part 2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,40 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Vedant"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "navratan",
+            lastname: "pandit"
+        }
+    }
+}
+
+// console.log(regUser.fullname.userfullname.lastname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = Object.assign({},obj1,obj2) // {} optional but highly recommendable
+// console.log(obj3);
+
+const obj4 = {...obj1,...obj2,...obj3}
+// console.log(obj4);
+
+
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty("isLoggedIn"))
+
+
+
