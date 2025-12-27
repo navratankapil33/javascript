7e3de79804bc5f48a24f38ab9e545e8999c2f9c3
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2025-12-27 21:01:05 +0000

    function

---
+++ 03_basics/function.js
@@ -0,0 +1,31 @@
+
+
+
+function sayMyName() {
+console.log("H")
+console.log("I")
+console.log("T")
+console.log("E")
+console.log("S")
+console.log("H")
+}
+
+//sayMyName()
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1+number2)
+// }
+function addTwoNumbers(number1, number2) {
+    let result = number1+number2
+    console.log("Hitesh")
+    return result
+}
+const result = addTwoNumbers(3,5)
+console.log("Result: ",result)
+
+function loginusername(username) {
+    return `${username} just logged in` 
+}
+
+let fetchh = loginusername("hitesh")
+console.log(fetchh)
