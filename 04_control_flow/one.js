aa938137d0002b0a3562807162d62836fc587e76
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2026-01-28 05:26:12 +0000

    controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+// // if
+
+// const isUserLoggedIn = true
+
+// if (isUserLoggedIn) {
+//     console.log("Hurray! User is logged in.");
+// }
+
+// if (2==="2") {
+//     console.log("If Executed");
+// } else {
+//     console.log("Else Executed");
+    
+// }
+
+// const temperature = 41
+// if (temperature < 50) {
+//     console.log("less than 50");
+// } else {
+//     console.log("greater than 50");
+    
+// }
+// console.log("Executed")
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+// //console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userloggedIn = true
+const debitCard = true
+
+const userloggedinfromGoogle = false
+const userloggedinfromGmail = true
+
+if (userloggedIn && debitCard) {
+    console.log("Allow to buy course.");
+    
+}
+
+if (userloggedinfromGoogle || userloggedinfromGmail) {
+    console.log("User logged in");
+    
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,42 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+    
+}
+
+// falsy values
+
+//false, 0, -0, BigInt -> 0n, "", null, undefined, NaN
+
+// truthy values
+
+// "0",'false', true,[],{}, function() {}
+
+if (userEmail.length == 0) {
+    console.log("Array is empty");
+}
+
+const empObj = {}
+
+if (Object.keys(empObj).length == 0) {
+    console.log("Object is empty");
+    
+}
+
+// Nullish coalescing operator(??): null undefined
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+
+
+console.log(val1);
+
+// Ternary operator
+
+const price = 90
+price <=70 ? console.log("less than 70"): console.log("greater than 70");
+
