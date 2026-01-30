f141cbb276909817854f3656b5526b40132a0fb9
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2026-01-30 17:58:26 +0000

    whileanddowhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,11 @@
+// let idx = 0
+// while (idx <= 10) {
+//     console.log(`Value of index is ${idx}`);
+//     idx++;
+// }
+
+let score = 11
+do {
+    console.log(`value of score is ${score}`)
+    score++
+} while( score <= 10)
