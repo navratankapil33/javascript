ac336d472cf727297e80b5fcbaa4d1a963b93028
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2026-02-01 13:52:32 +0000

    forEachArrayObject

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -5,5 +5,35 @@ const coding = ["java", "cpp", "js", "python"]
 // })
 
 coding.forEach( (val) => {
-    console.log(val)
+  //  console.log(val)
+})
+
+function printItem (item) {
+    //console.log(item);
+    
+}
+
+coding.forEach(printItem);
+
+coding.forEach((item,index,arr) => {
+    //console.log(item,index,arr);
+    
+})
+
+const myCoding = [ {
+    languageName: "javascript",
+    languageFile: "js"
+}
+,{
+    languageName: "java",
+    languageFile: "java"
+}, {
+    languageName: "python",
+    languageFile: "py"
+}
+]
+
+myCoding.forEach((item)=> {
+    console.log(item.languageName);
+    
 })
