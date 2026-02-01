eb814b17d3d49071238f335b8e0b847d646145cb
navratankapil33 <73329074+navratankapil33@users.noreply.github.com>
2026-02-01 07:02:55 +0000

    mapnotiterableforin

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -11,16 +11,27 @@ const myObject = {
 // }
 
 for (const key in myObject) {
-    console.log(key);
+    //console.log(key);
 }
 for (const key in myObject) {
-    console.log(myObject[key]);
+    //console.log(myObject[key]);
 }
 
 const programming = ["js","ruby","java","py","cpp"]
 for (const key in programming) {
-    console.log(key);
+    //console.log(key);
 }
 for (const key in programming) {
-    console.log(programming[key]);
+    //console.log(programming[key]);
+}
+
+const map = new Map();
+
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('USSR',"Russia")
+map.set('CHN',"CHina")
+
+for (const key in map) {
+    console.log(key);
 }
